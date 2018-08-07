3fa04a419c242a201266089f0715b7d889f5fb92
jlgzb <gzbcumtb@163.com>
2018-08-07 14:34:46 +0800

    upload the final pngs and the corresponding R scripts

--- plot4.R
+++ plot4.R
@@ -50,7 +50,8 @@ legend("topright",
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col = c("black", "red", "blue"), 
        lty = 1,
-       lwd = 2.5)
+       lwd = 2.5,
+       bty = "n")
 
 plot(datetime, df_2007$Global_reactive_power,
      xlab = "datetime",
